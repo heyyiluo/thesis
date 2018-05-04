ec61c27a58c8e3dee6eb94ce33055e95576b4236
luoyi3229 <luoy99@hotmail.com>
2018-05-04 15:26:22 +0200

    Revert "Revert "2.0""
    This reverts commit 3c55304c8b11fe5b5ed022c494e17d7b581a9390.

--- web/thesisweb/__init__.py
+++ web/thesisweb/__init__.py
@@ -1,9 +1,12 @@
 import os
 from flask import Flask
+from datetime import timedelta
 
 
 def create_app(*args, **kwargs):
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True, static_url_path='/static')
+    app.debug=True
+    
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'thesis.sqlite'),
@@ -15,7 +18,9 @@ def create_app(*args, **kwargs):
         pass
 
     app.config.from_pyfile('config.py', silent=True)
-
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = timedelta(seconds=1)
+    app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(seconds=1)
+    
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
